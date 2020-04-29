2678e8c0b55a60dc5a12750154f0ceb03356773b
Zulfi Izzulhaq <zulfi.izzulhaq@gmail.com>
2020-04-29 22:39:42 +0700

    create view informasi ujian

--- resources/views/pengaturan.blade.php
+++ resources/views/pengaturan.blade.php
@@ -6,7 +6,7 @@
 
 @section('content')
     <div class="row">
-      <div class="col-4">
+      <div class="col-md-4">
         <div class="card">
           <div class="card-header">
             <h4 class="card-title">Informasi Aplikasi</h4>
@@ -15,18 +15,18 @@
             <form action="">
               <div class="form-group">
                 <label for="form-judul">Title</label>
-                <input type="text" class="form-control">
+                <input type="text" class="form-control" placeholder="Masukkan Title Aplikasi">
               </div>
               <div class="form-group">
                 <label for="form-deskripsi">Deskripsi</label>
-                <input type="text" class="form-control">
+                <input type="text" class="form-control" placeholder="Masukkan Deskripsi Aplikasi">
               </div>
               <div class="form-group">
-                <label for="form-logo">Logo</label>
-                <div class="current-logo">
-                  <h1><i class="
-                    fas fa-cloud-upload-alt"></i></h1>
-                  <img src="" alt="">
+                <label for="form-logo">Logo Aplikasi</label>
+                <div class="current-logo mb-3">
+                  <!-- <h1><i class="
+                    fas fa-cloud-upload-alt"></i></h1> -->
+                  <img src="https://i.pinimg.com/originals/a3/1b/1d/a31b1d35380af24948f54b42346225f6.png" width="150px" alt="">
                 </div>
                 <input type="file" name="" id="" class="form-control">
               </div>
@@ -37,7 +37,20 @@
           </div>
         </div>
       </div>
-      <div class="col-8">
+      <div class="col-md-8">
+        <div class="card">
+          <div class="card-header">
+            <h4 class="card-title">Informasi Ujian</h4>
+          </div>
+          <div class="card-body">
+            <textarea name="informasi_ujian" id="info-ujian" cols="30" rows="10"></textarea>
+          </div>
+          <div class="card-footer">
+            <button class="btn btn-sm btn-success">Simpan</button>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-12">
         <div class="card">
           <div class="card-header">
             <div class="card-head-row">
@@ -59,6 +72,71 @@
                 </tr>
               </thead>
               <tbody>
+                <tr>
+                  <td>1.</td>
+                  <td>Muhammad Zulfi Izzulhaq</td>
+                  <th>zulfi.izzulhaq@gmail.com</th>
+                  <th>
+                    <span class="badge badge-danger">Admin</span>
+                    <span class="badge badge-warning">Petugas Soal</span>
+                    <span class="badge badge-info">Petugas Ujian</span>
+                  </th>
+                  <th>
+                    <button class="btn btn-sm btn-danger">Hapus</button>
+                  </th>
+                </tr>
+                <tr>
+                  <td>1.</td>
+                  <td>Muhammad Zulfi Izzulhaq</td>
+                  <th>zulfi.izzulhaq@gmail.com</th>
+                  <th>
+                    <span class="badge badge-danger">Admin</span>
+                    <span class="badge badge-warning">Petugas Soal</span>
+                    <span class="badge badge-info">Petugas Ujian</span>
+                  </th>
+                  <th>
+                    <button class="btn btn-sm btn-danger">Hapus</button>
+                  </th>
+                </tr>
+                <tr>
+                  <td>1.</td>
+                  <td>Muhammad Zulfi Izzulhaq</td>
+                  <th>zulfi.izzulhaq@gmail.com</th>
+                  <th>
+                    <span class="badge badge-danger">Admin</span>
+                    <span class="badge badge-warning">Petugas Soal</span>
+                    <span class="badge badge-info">Petugas Ujian</span>
+                  </th>
+                  <th>
+                    <button class="btn btn-sm btn-danger">Hapus</button>
+                  </th>
+                </tr>
+                <tr>
+                  <td>1.</td>
+                  <td>Muhammad Zulfi Izzulhaq</td>
+                  <th>zulfi.izzulhaq@gmail.com</th>
+                  <th>
+                    <span class="badge badge-danger">Admin</span>
+                    <span class="badge badge-warning">Petugas Soal</span>
+                    <span class="badge badge-info">Petugas Ujian</span>
+                  </th>
+                  <th>
+                    <button class="btn btn-sm btn-danger">Hapus</button>
+                  </th>
+                </tr>
+                <tr>
+                  <td>1.</td>
+                  <td>Muhammad Zulfi Izzulhaq</td>
+                  <th>zulfi.izzulhaq@gmail.com</th>
+                  <th>
+                    <span class="badge badge-danger">Admin</span>
+                    <span class="badge badge-warning">Petugas Soal</span>
+                    <span class="badge badge-info">Petugas Ujian</span>
+                  </th>
+                  <th>
+                    <button class="btn btn-sm btn-danger">Hapus</button>
+                  </th>
+                </tr>
                 <tr>
                   <td>1.</td>
                   <td>Muhammad Zulfi Izzulhaq</td>
@@ -126,5 +204,16 @@
 @endsection
 
 @section('script')
-    
+    <script src="{{ asset('assets/ckeditor/ckeditor.js') }}"></script>
+    <script>
+      var table_admin = $('#table-admin').DataTable()
+
+      CKEDITOR.replace('info-ujian', {
+        height: '259px',
+        filebrowserImageBrowseUrl: '{{ url("/filemanager?type=Images") }}',
+        filebrowserImageUploadUrl: '{{ url("/filemanager/upload?type=Images&_token=") }}',
+        filebrowserBrowseUrl: '{{ url("/filemanager?type=Files") }}',
+        filebrowserUploadUrl: '{{ url("/filemanager/upload?type=Files&_token") }}='
+      })
+    </script>
 @endsection
